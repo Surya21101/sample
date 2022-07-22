95fa394e4dff668f6dba8b912420a08583977918
Surya21101 <96860185+Surya21101@users.noreply.github.com>
2022-07-22 12:28:56 +0530

    Add files via upload

---
+++ com.Dao/EmpFreshDao.java
@@ -0,0 +1,8 @@
+package com.Dao;
+
+public interface EmpFreshDao {
+	public void CreateEmp();
+	public void UpdateEmp();
+	public void DeleteEmp();
+	public void ShowEmp();
+}
---
+++ com.Dao/EmpSalDao.java
@@ -0,0 +1,8 @@
+package com.Dao;
+
+public interface EmpSalDao {
+	public void CreateSal();
+	public void UpdateSal();
+	public void DeleteSal();
+	public void ShowSal();
+}
